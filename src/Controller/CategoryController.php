afc8b7039ad307e418c6a6ce6e0286c7eb3239d1
cchapleau <ch.chapleau@gmail.com>
2020-12-03 20:23:38 +0100

    Quest 08 - Symfony : Récupérer des données stockées avec Doctrine

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Program;
+use App\Entity\Category;
+
+/**
+* @Route("/categories", name="category_")
+*/
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        $category = $this->getDoctrine()
+             ->getRepository(Category::class)
+             ->findAll();
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+            'No category found in category\'s table.'
+            );
+        }
+
+        return $this->render('category/index.html.twig',
+            ['categories' => $category]
+        );
+    }
+
+    /**
+     * @Route("/{categoryName}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(string $categoryName): Response
+    {
+        if (!$categoryName) {
+            throw $this
+                ->createNotFoundException(
+                    'No '.$categoryName.' has been sent to find a category in category\'s table.'
+                );
+        }
+
+        $category = $this->getDoctrine()
+        ->getRepository(Category::class)
+        ->findBy(['name' => $categoryName]);
+
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(
+                ['category' => $category],
+                ['id' => 'DESC'],
+                3
+            );
+
+        return $this->render('category/show.html.twig', [
+            'categoryName' => $categoryName,
+            'programs' => $programs]);
+    }
+}
---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Program;
+use App\Entity\Category;
+
+/**
+* @Route("/programs", name="program_")
+*/
+
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+             ->getRepository(Program::class)
+             ->findAll();
+
+        return $this->render('program/index.html.twig',
+            ['programs' => $programs]
+        );
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(int $id):Response
+    {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+        ]);
+    }
+}
