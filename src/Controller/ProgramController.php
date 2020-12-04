b96b6579aa555959dd67ac38ce0d4d8f9427a6a2
cchapleau <ch.chapleau@gmail.com>
2020-12-04 07:21:14 +0100

    Quest 09 - Symfony : Les relations bidirectionnelles avec Doctrine

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
 use App\Entity\Category;
+use App\Entity\Season;
 
 /**
 * @Route("/programs", name="program_")
@@ -31,7 +32,7 @@ class ProgramController extends AbstractController
     /**
      * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
      */
-    public function show(int $id):Response
+    public function show(int $id) :Response
     {
         $program = $this->getDoctrine()
             ->getRepository(Program::class)
@@ -42,8 +43,40 @@ class ProgramController extends AbstractController
                 'No program with id : '.$id.' found in program\'s table.'
             );
         }
+
+        $seasons = $program->getSeasons();
+
         return $this->render('program/show.html.twig', [
             'program' => $program,
+            'seasons' => $seasons,
+        ]);
+    }
+
+    /**
+     * @Route("/{programId}/seasons/{seasonId}", methods={"GET"}, name="season_show")
+     */
+    public function showSeason(int $programId, int $seasonId):Response
+    {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $programId]);
+
+        $seasons = $this->getDoctrine()
+            ->getRepository(Season::class)
+            ->findOneBy(['id' => $seasonId]);
+
+        $episodes = $seasons->getEpisodes();
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $seasons,
+            'episodes' => $episodes,
         ]);
     }
 }
